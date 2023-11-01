71fa971a622212da3fda5aa4e4750dcd0d546ae3
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-01 17:09:24 +0300

    погнали

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+  res.send("123");
+});
+
+app.listen(4444, () => {
+  console.log("Hello Express.js");
+});
