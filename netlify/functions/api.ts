34bee6610c3118e627d3af0f4b020c42c8b6cb6b
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-02 14:45:21 +0300

    правка импортов

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import usersRoute from "../../src/routes/user.route";
-import dialogsRoute from "../../src/routes/user.route";
-import messagesRoute from "../../src/routes/user.route";
+import dialogsRoute from "../../src/routes/dialog.route";
+import messagesRoute from "../../src/routes/message.route";
 import serverless from "serverless-http";
 
 import cors from "cors";
