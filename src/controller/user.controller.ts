7d50108e457a591fd3689c754bdde65482a9a0dd
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-03 13:19:37 +0300

    добавлена регистрация и авторизация + небольшие проверки

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,15 +1,27 @@
 import database from "../utils/database";
 import { Request, Response } from "express";
-import { hashPassword } from "../utils/password.hash";
+import { checkPassword, hashPassword } from "../utils/password.hash";
 class UserController {
   async createUser(req: Request, res: Response) {
     const { firstName, lastName, username, login, password } = req.body;
-    const passwordHash = await hashPassword(password);
-    const newUser = await database.query(
-      "insert into users (first_name, last_name, username, login, password_hash) values ($1, $2, $3, $4, $5) returning *",
-      [firstName, lastName, username, login, passwordHash],
-    );
-    res.json(newUser.rows[0]);
+    const user = await database.query("select * from users where login = $1", [
+      login,
+    ]);
+    if (!user.rows[0]) {
+      const passwordHash = await hashPassword(password);
+      const newUser = await database.query(
+        "insert into users (first_name, last_name, username, login, password_hash) values ($1, $2, $3, $4, $5) returning *",
+        [firstName, lastName, username, login, passwordHash],
+      );
+      res.json({
+        ...newUser.rows[0],
+        password,
+      });
+    } else {
+      res.json({
+        message: "Такой пользователь уже есть в системе",
+      });
+    }
   }
 
   async getUsers(req: Request, res: Response) {
@@ -27,13 +39,25 @@ class UserController {
 
   async updateUser(req: Request, res: Response) {
     const { firstName, lastName, username, login, password, id } = req.body;
-    const passwordHash = await hashPassword(password);
-    const user = await database.query(
-      "update users set first_name = $1, last_name = $2, username = $3, login = $4, password_hash = $5 where id = $6 returning *",
-      [firstName, lastName, username, login, passwordHash, id],
+    const userLogin = await database.query(
+      "select * from users where login = $1",
+      [login],
     );
-    console.log(user.rows[0]);
-    res.json(user.rows[0]);
+    if (!userLogin.rows[0]) {
+      const passwordHash = await hashPassword(password);
+      const user = await database.query(
+        "update users set first_name = $1, last_name = $2, username = $3, login = $4, password_hash = $5 where id = $6 returning *",
+        [firstName, lastName, username, login, passwordHash, id],
+      );
+      res.json({
+        ...user.rows[0],
+        password,
+      });
+    } else {
+      res.json({
+        message: "такой логин уже занят",
+      });
+    }
   }
 
   async deleteUser(req: Request, res: Response) {
@@ -43,6 +67,24 @@ class UserController {
       [user_id],
     );
     res.json(user.rows[0]);
+  }
+
+  async checkUser(req: Request, res: Response) {
+    const { login, password } = req.body;
+    const user = await database.query("select * from users where login = $1", [
+      login,
+    ]);
+    const userPasswordHash = user.rows[0]["password_hash"];
+    if (await checkPassword(password, userPasswordHash)) {
+      res.json({
+        ...user.rows[0],
+        password,
+      });
+    } else {
+      res.json({
+        message: "пользователь неверный",
+      });
+    }
   }
 }
 
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,12 +2,13 @@ import Router from "express";
 import userController from "../controller/user.controller";
 
 // @ts-ignore
-const usersRoute  = new Router();
+const usersRoute = new Router();
 
 usersRoute.get("/users", userController.getUsers);
 usersRoute.get("/users/:id", userController.getUser);
 usersRoute.post("/users", userController.createUser);
 usersRoute.put("/users", userController.updateUser);
 usersRoute.delete("/users/:id", userController.deleteUser);
+usersRoute.post("/users/check", userController.checkUser);
 
 export default usersRoute;
